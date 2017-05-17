b87a03f80c6dae5cffa7a88db7bdb31bf684ef57
hurjaehoe <kokoru58@hanmail.net>
2017-05-17 12:48:14 +0900

    exception 처리

---
+++ src/main/java/com/roamstory/common/CommonExceptionAdvice.java
@@ -0,0 +1,31 @@
+package com.roamstory.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class CommonExceptionAdvice {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
+//	
+//	@ExceptionHandler(Exception.class)
+//	public String common(Exception e) {
+//		logger.info(e.toString());
+//		
+//		return "error_common";
+//	}
+//	
+	@ExceptionHandler(Exception.class)
+	private ModelAndView errorModelAndView(Exception excetion) {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/common/error_common");
+		modelAndView.addObject("exception", excetion);
+		
+		return modelAndView;
+	}
+	
+}
