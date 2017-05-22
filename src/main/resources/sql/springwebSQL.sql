0840db8cfdbd075642a15a8f8ba78317a4501876
hurjaehoe <kokoru58@hanmail.net>
2017-05-22 23:38:59 +0900

    SQL문 추가

--- src/main/resources/sql/springwebSQL.sql
+++ src/main/resources/sql/springwebSQL.sql
@@ -37,3 +37,33 @@ insert into tbl_board (title,content,writer)
 (select title,content,writer from tbl_board);
 
 select count(*) from tbl_board;
+
+create table tbl_user ( 
+ uid varchar(50) NOT NULL, 
+ upw varchar(50) NOT NULL,
+ uname varchar(100) NOT NULL,
+ upoint int NOT NULL DEFAULT 0,
+ primary key(uid)
+);
+
+create table tbl_message (
+ mid int not null auto_increment,
+ targetid varchar(50) not null, 
+ sender varchar(50) not null, 
+ message text not null,
+ opendate timestamp,
+ senddate timestamp not null default now(),
+ primary key(mid)
+);
+
+alter table tbl_message add constraint fk_usertarget 
+foreign key (targetid) references tbl_user (uid); 
+
+alter table tbl_message add constraint fk_usersender 
+foreign key (targetid) references tbl_user (uid); 
+
+insert into tbl_user(uid, upw, uname) values ('user00','user00','IRON MAN');
+insert into tbl_user(uid, upw, uname) values ('user01','user01','CAPTAIN');
+insert into tbl_user(uid, upw, uname) values ('user02','user02','HULK');
+insert into tbl_user(uid, upw, uname) values ('user03','user03','Thor');
+insert into tbl_user(uid, upw, uname) values ('user10','user10','Quick Silver');
